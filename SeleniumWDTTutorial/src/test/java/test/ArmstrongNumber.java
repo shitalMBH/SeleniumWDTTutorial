4d37007ad62a26648514cecd26a9720ea00b8d48
Admin <Admin@DESKTOP-SS9R2SM>
2024-12-07 17:09:27 +0530

    file updated with correct code after use of int digit

--- SeleniumWDTTutorial/src/test/java/test/ArmstrongNumber.java
+++ SeleniumWDTTutorial/src/test/java/test/ArmstrongNumber.java
@@ -8,7 +8,7 @@ public class ArmstrongNumber {
 		int digitCount = String.valueOf(num).length();
 		for (int temp = 0; temp != 0; temp /= 10) {
 			int digit = num % 10;
-			sum +=(int) Math.pow(temp, digitCount);
+			sum +=(int) Math.pow(digit, digitCount);
 		}
 		if (sum == num) {
 			System.out.println(num + " is an Armstrong number");
