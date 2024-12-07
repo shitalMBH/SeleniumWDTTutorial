cd313d9187d8aafc864eae6f3d55000414505600
Admin <Admin@DESKTOP-SS9R2SM>
2024-12-07 17:01:50 +0530

    new file for armstrong number

---
+++ SeleniumWDTTutorial/src/test/java/test/ArmstrongNumber.java
@@ -0,0 +1,22 @@
+package test;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		int num = 153;
+		int sum = 0;
+		int digitCount = String.valueOf(num).length();
+		for (int temp = 0; temp != 0; temp /= 10) {
+			int digit = num % 10;
+			sum +=(int) Math.pow(temp, digitCount);
+		}
+		if (sum == num) {
+			System.out.println(num + " is an Armstrong number");
+		} else {
+			System.out.println(num + " is an not Armstrong number");
+
+		}
+
+	}
+
+}
